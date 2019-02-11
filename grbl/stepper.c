433d1aa9d97193392dcc902650a249406807a2da
Marcin Kaluza <mk@flex.pm>
2019-02-11 19:03:13 +0100

    RAMPS: fix step port setting error

--- grbl/stepper.c
+++ grbl/stepper.c
@@ -337,8 +337,8 @@ ISR(TIMER1_COMPA_vect)
       st.step_bits[2] = (STEP_PORT(2) & ~(1 << STEP_BIT(2))) | st.step_outbits[2]; // Store out_bits to prevent overwriting.
       */
     #else
-      STEP_PORT(0) = (STEP_PORT(0) & ~(1 << STEP_BIT(0))) | (st.step_outbits & (X_STEP_MASK | Y_STEP_MASK));
-      STEP_PORT(2) = (STEP_PORT(2) & ~(1 << STEP_BIT(2))) | (st.step_outbits & Z_STEP_MASK);
+      STEP_PORT(0) = (STEP_PORT(0) & ~(X_STEP_MASK | Y_STEP_MASK)) | (st.step_outbits & (X_STEP_MASK | Y_STEP_MASK));
+      STEP_PORT(2) = (STEP_PORT(2) & ~(Z_STEP_MASK)) | (st.step_outbits & Z_STEP_MASK);
     #endif
   #else  
     #ifdef STEP_PULSE_DELAY
@@ -471,8 +471,8 @@ ISR(TIMER1_COMPA_vect)
   // Reset stepping pins (leave the direction pins)
 step_off:
   #ifdef DEFAULTS_RAMPS_BOARD
-    STEP_PORT(0) = (STEP_PORT(0) & ~(1 << STEP_BIT(0))) | (step_port_invert_mask & (X_STEP_MASK | Y_STEP_MASK));
-    STEP_PORT(2) = (STEP_PORT(2) & ~(1 << STEP_BIT(2))) | (step_port_invert_mask & Z_STEP_MASK);
+    STEP_PORT(0) = (STEP_PORT(0) & ~(X_STEP_MASK | Y_STEP_MASK)) | (step_port_invert_mask & (X_STEP_MASK | Y_STEP_MASK));
+    STEP_PORT(2) = (STEP_PORT(2) & ~(Z_STEP_MASK)) | (step_port_invert_mask & Z_STEP_MASK);
   #else
     STEP_PORT = (STEP_PORT & ~STEP_MASK) | (step_port_invert_mask & STEP_MASK);
   #endif // Ramps Board
